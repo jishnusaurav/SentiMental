178124aaf3ad2dd0356256dcfc2cdea86a7e937f
jishnusaurav <jishnusaurav@gmail.com>
2020-05-22 01:32:29 +0530

    first commit

---
+++ bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/jishnu/Desktop/Sentiment-Analysis-Django-Web-App-master/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
